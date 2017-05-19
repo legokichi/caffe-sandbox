a29846f3fc7c193a2a97815509cd849d1a1b41f0
legokichi <legokichi@gmail.com>
2017-05-19 06:27:37 +0000

    init

---
+++ download_vgg16.sh
@@ -0,0 +1,12 @@
+wget http://www.robots.ox.ac.uk/~vgg/software/very_deep/caffe/VGG_ILSVRC_16_layers.caffemodel
+wget https://gist.githubusercontent.com/ksimonyan/211839e770f7b538e2d8/raw/ded9363bd93ec0c770134f4e387d8aaaaa2407ce/VGG_ILSVRC_16_layers_deploy.prototxt
+
+
+
+
+
+
+
+
+
+
